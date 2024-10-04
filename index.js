4ef5ace3115f51798360e0dfd0e476a007c7d9d9
Khalil <khalil5614@gmail.com>
2024-10-04 17:24:54 +0600

    api implemented

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Assignment 4 server running");
+app.get("/api/courses", (req, res) => {
+  const list = require("./data/courseDetails.json");
+  res.send(list);
+});
+app.get("/api/courses/:id", (req, res) => {
+  const courseList = require("./data/courseDetails.json");
+  const params = req.params;
+  const { id } = params;
+  //const course = courseList.find((course) => parseInt(course.course_id) == id);
+  const course = courseList.find(function (element) {
+    const course_id = parseInt(element.course_id);
+    return course_id == id;
+  });
+
+  res.send(course);
 });
 
 app.listen(port, () => {
